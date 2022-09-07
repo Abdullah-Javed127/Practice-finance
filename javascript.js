67596ab73427adff4b14c5081bd853937bf91605
Abdullah-Javed127 <abdullah127c@gmail.com>
2022-09-08 02:01:08 +0500

    final sum not adding
    JS issue about redeclaring variables of "save" and "finalSave"

--- javascript.js
+++ javascript.js
@@ -3,16 +3,29 @@ let saveBtn=document.getElementById("save-button1");
 let list=document.getElementById("list");
 let EnterBtn="";
 let save=[];
-
+let finalSave="";
+EnterBtn=document.getElementById("list");
 
 saveBtn.addEventListener("click",function()
     {
         for(let x=0;x<inputFloor.value;x++)
         {
-            save+=`<input><br>`
+            save[x]+=`<input id=inputJS><br>`;
         }
-        EnterBtn=`<button onclick="button()">save</button>`
-
+        EnterBtn=`<button onclick="button()">save</button>`;
         list.innerHTML=save+EnterBtn;
+        inputFloor.value=null;
     }
 )
+
+
+function button()
+{
+    finalSave;
+    for(let i=0;i<inputFloor.value;i++)
+    {
+        save[i].value=save[i].value+finalSave;
+    }
+    alert(save[1].value);
+}
+
