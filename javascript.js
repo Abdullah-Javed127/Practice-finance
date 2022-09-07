bb2b9ecfec8affad6ea073e23b6f782976897bf4
Abdullah-Javed127 <abdullah127c@gmail.com>
2022-09-08 00:59:19 +0500

    no sum of rent
    no sum of rent

--- javascript.js
+++ javascript.js
@@ -1,41 +1,18 @@
-let inputFloor=document.getElementById("input-floors");
-let inputRent=document.getElementById("input-rent");
-let saveBtn=document.getElementById("save");
+let inputFloor=document.getElementById("input-floor");
+let saveBtn=document.getElementById("save-button1");
 let list=document.getElementById("list");
-let sum;
-let save=[];//array
+let EnterBtn="";
+let save=[];
+
 
 saveBtn.addEventListener("click",function()
-{
-    render();
-})
-
-function render()
-{
-    let button="";
-    for(let x=1;x<=inputFloor.value;x++)
     {
-        save+=`<input id=inputJS placeholder=Rent${x}><br>` //id here links to CSS
+        for(let x=0;x<inputFloor.value;x++)
+        {
+            save+=`<input><br>`
+        }
+        EnterBtn=`<button onclick="button()">save</button>`
+
+        list.innerHTML=save+EnterBtn;
     }
-    button=`<button onclick="total()">save</button>`; //onclick links to total function
-    list.innerHTML=save+button;
-}
-
-let inputForRent=document.getElementById("list");
-let save2Btn=document.getElementById("list");
-
-save2Btn.innerHTML=button;//*
-
-save2Btn.addEventListener("click",function()
-{
-    total();
-})
-
-function total()//issue is here
-{
-    for(let x=0;x<save.length;x++)
-    {
-        sum=save[x]+sum;
-    }
-    alert(sum);
-}
+)
